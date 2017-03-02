6f729a5f878b3e7056ccfe9ef5b3484e89cc2fcc
song <30733705@qq.com>
2017-03-02 16:06:31 +0800

    add templates and lib

---
+++ app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-3-2 下午8:17
+# @Author  : Sugare
+# @mail    : 30733705@qq.com
+# @File    : app.py
+# @Software: PyCharm
+
+
+import tornado.web
+import tornado.ioloop
+import tornado.httpserver
+import tornado.options
+import os
+from lib.orm import noblog,rightbarDate,rightbarHot,latest,dateData,PagerPerItem, PagerTotalItem, getData
+
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/?', IndexHandler),
+            (r'/page/(\d+)/?', PageHandler),
+            (r'/tag/([a-z]+$)', TagHandler),
+            (r'/.*', ErrorHandler),
+        ]
+
+        settings = dict(
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            debug=True
+        )
+
+        super(Application,self).__init__(handlers, **settings)
+
+
+
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    pass
+
+class IndexHandler(BaseHandler):
+    def get(self):
+        perPage = 8
+        totalItem = PagerTotalItem()
+        totalPage = totalItem // perPage if totalItem % perPage == 0 else totalItem // perPage + 1
+
+        dataPool = PagerPerItem(1, perPage)
+        print(dataPool[1].get_tags())
+        self.render('index.html', dataPool=dataPool, rightbarHot=rightbarHot(), rightbarDate=rightbarDate())
+
+class PageHandler(BaseHandler):
+    def get(self, blog_id):
+        try:
+            dataPool = getData(blog_id)
+        except:
+            self.write_error(404)
+        self.render('page.html',dataPool=dataPool, rightbarHot=rightbarHot(), rightbarDate=rightbarDate())
+
+class TagHandler(BaseHandler):
+    def get(self, tag):
+        self.write(tag)
+
+
+class ErrorHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write_error(404)
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
---
+++ lib/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-3-2 下午2:09
+# @Author  : Sugare
+# @mail    : 30733705@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
